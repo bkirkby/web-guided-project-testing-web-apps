9df75d2de9910b3e0c2b6fd4c911357164ada9af
kirkby <kirkby@neighbor.com>
2021-05-12 21:04:37 -0600

    progress!!

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 
 import AnimalForm from './AnimalForm';
 
+import { lang } from '../App.test';
+
 test("AnimalForm renders", () => {
   render(<AnimalForm />);
 })
