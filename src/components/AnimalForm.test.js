1779443aef23d59ff4a13eaa3286b968bd5e3dfa
kirkby <kirkby@neighbor.com>
2021-05-12 21:09:36 -0600

    progress!!

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
+import userEvent from '@testing-library/user-event';
 import AnimalForm from './AnimalForm';
 
 import { lang } from '../App.test';
 
 test("AnimalForm renders", () => {
   render(<AnimalForm lang={lang} />);
+})
+
+test("submited form renders animal name", () => {
+  render(<AnimalForm lang={lang} />)
+
+  const speciesInput = screen.getByLabelText(/species/i)
+
+  userEvent.type('Big Bear');
+
+  expect(speciesInput).toHaveValue('Big Bear');
 })
