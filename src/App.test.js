97eb2d590275570ba2bf623fb8bdff98bc35d92b
kirkby <kirkby@neighbor.com>
2021-05-12 20:28:10 -0600

    progress!!

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,8 @@ import App from './App';
 // Arrange, Act, Assert
 
 // test("bogus test", () => {
-describe("", () => {
-  it("bogus test", () => {
+describe("bogus", () => {
+  it("checks if true is truthy", () => {
     expect(true).toBeTruthy()
   })
 })
