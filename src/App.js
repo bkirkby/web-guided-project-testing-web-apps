d9468ff4baea1d14f66ee36b1936b7d1f663ef78
kirkby <kirkby@neighbor.com>
2021-05-12 21:00:06 -0600

    progress!!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App(props) {
   return (
     <div className="App">
-      <h1>Add New Animal</h1>
+      <h1>Add NEw Animal</h1>
       <AnimalForm lang={props.lang} />
     </div>
   );
